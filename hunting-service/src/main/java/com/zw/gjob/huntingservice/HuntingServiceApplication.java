88af8af5025740ad9d955d6987a2eb75c1c55f36
王凤晨 <823639686@qq.com>
2019-11-28 18:25:28 +0800

    debug

--- hunting-service/src/main/java/com/zw/gjob/huntingservice/HuntingServiceApplication.java
+++ hunting-service/src/main/java/com/zw/gjob/huntingservice/HuntingServiceApplication.java
@@ -2,8 +2,12 @@ package com.zw.gjob.huntingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class HuntingServiceApplication {
 
     public static void main(String[] args) {
