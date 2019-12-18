78f1caed28a8993292f490afff47fad1a223281b
王凤晨 <823639686@qq.com>
2019-12-18 16:51:50 +0800

    commons

---
+++ commons/src/main/java/com/zw/gjob/commons/model/ApiResult.java
@@ -0,0 +1,33 @@
+package com.zw.gjob.commons.model;
+
+import com.zw.gjob.commons.constants.ResultCodeConstants;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+public class ApiResult {
+    private Integer code;
+    private String msg;
+    private Object data;
+
+    public static ApiResult success() {
+        return new ApiResult().setCode(ResultCodeConstants.SUCCESS_CODE).setMsg("成功");
+    }
+
+    public static ApiResult success(String msg) {
+        return new ApiResult().setCode(ResultCodeConstants.SUCCESS_CODE).setMsg(msg);
+    }
+
+    public static ApiResult error() {
+        return new ApiResult().setCode(ResultCodeConstants.ERROR_CODE).setMsg("失败");
+    }
+
+    public static ApiResult error(String msg) {
+        return new ApiResult().setCode(ResultCodeConstants.ERROR_CODE).setMsg(msg);
+    }
+
+    public static ApiResult other(Integer code,String msg){
+        return new ApiResult().setCode(code).setMsg(msg);
+    }
+}
